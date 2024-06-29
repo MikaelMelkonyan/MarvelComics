35c037a94aae04fbe75237a9c71d2c0a511f5b92
Mikael <melkonyan2311@icloud.com>
2024-06-29 15:12:20 -0700

    WIP

---
+++ MarvelComics/MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Mikael Melkonyan on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
